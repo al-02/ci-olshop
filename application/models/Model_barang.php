d3cda2073fc93dc7cf9cb84c6b5c3ef7438d9eb1
al-02 <63692674+al-02@users.noreply.github.com>
2020-04-23 11:24:16 +0700

    Revisi 1

--- application/models/Model_barang.php
+++ application/models/Model_barang.php
@@ -13,8 +13,26 @@ class Model_barang extends CI_Model{
 		return $this->db->get_where($table,$where);
 	}
 
-	public function update_data($where,$data,$table){
-		return $this->db->where($where);
-		return $this->db->update($table,$data);
+	public function update_data($where,$data,$table)
+	{
+		$this->db->where($where);
+		$this->db->update($table,$data);
+	}
+
+	public function hapus_data($where,$table){
+		$this->db->where($where);
+		$this->db->delete($table);
+	}
+
+	public function find($id)
+	{
+		$result = $this->db->where('id_brg', $id)
+						   ->limit(1)
+						   ->get('tb_barang');
+		if($result->num_rows() > 0){
+			return $result->row();
+		}else{
+			return array();
+		}
 	}
 }
