a61d0c4fef0d887567f6cc3083c672d3bb27c0d6
al-02 <63692674+al-02@users.noreply.github.com>
2020-06-02 23:52:42 +0700

    Revisi 2

--- application/views/pembayaran.php
+++ application/views/pembayaran.php
@@ -1,3 +1,74 @@
+<<<<<<< HEAD
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-md-2"></div>
+		<div class="col-md-8">
+			<div class="btn btn-sm btn-success">
+				<?php
+				$grand_total = 0;
+				if ($keranjang = $this->cart->contents())
+				{
+					foreach ($keranjang as $items)
+					{
+						$grand_total = $grand_total + $items['subtotal'];
+					}
+
+				echo "<h4>Total Belanja Anda : Rp. ".number_format($grand_total, 0,',','.');
+				 ?>
+			</div><br><br>
+
+			<h3>Input Alamat Pengiriman dan Pembayaran</h3>
+
+			<form method="post" action="<?php echo base_url('dashboard/proses_pesanan') ?>">
+
+				<div class="form-group">
+					<label>Nama Lengkap</label>
+					<input type="text" name="nama" placeholder="Nama Lengkap Anda " class="form-control">
+				</div>
+
+				<div class="form-group">
+					<label>Alamat Lengkap</label>
+					<input type="text" name="alamat" placeholder="Alamat Lengkap Anda " class="form-control">
+				</div>
+
+				<div class="form-group">
+					<label>No. Telepon</label>
+					<input type="text" name="no.telp" placeholder="No. Telepon Anda " class="form-control">
+				</div>
+
+				<div class="form-group">
+					<label>Jasa Pengiriman</label>
+					<select class="form-control">
+						<option>JNE</option>
+						<option>TIKI</option>
+						<option>POS Indonesia</option>
+						<option>GOJEK</option>
+					</select>
+				</div>
+				
+				<div class="form-group">
+					<label>Pilih BANK</label>
+					<select class="form-control">
+						<option>BCA - XXXXXXXXX</option>
+						<option>BRI - XXXXXXXXX</option>
+						<option>BNI - XXXXXXXXX</option>
+						<option>MANDIRI - XXXXXXXXX</option>
+					</select>
+				</div>
+
+				<button type="submit" class="btn btn-sm btn-primary mb-3">Pesan</button>
+			</form>
+
+			<?php 
+		} else
+		{
+			echo "<h4>Keranjang Belanja Anda Masih Kosong";
+		}
+			 ?>
+		</div>
+		<div class="col-md-2"></div>
+	</div>
+=======
 <div class="container-fluid">
 	<div class="row">
 		<div class="col-md-2"></div>
@@ -67,4 +138,5 @@
 		</div>
 		<div class="col-md-2"></div>
 	</div>
+>>>>>>> cde0bab563ae1e09e10f672bf65e94f1e9f8d13e
 </div>
--- application/views/proses_pesanan.php
+++ application/views/proses_pesanan.php
@@ -1,5 +1,12 @@
+<<<<<<< HEAD
 <div class="container-fluid">
 	<div class="alert alert-success">
 		<p class="text-center align-middle">Selamat, Pesanan Anda telah Berhasil diproses!!!</p>
 	</div>
+=======
+<div class="container-fluid">
+	<div class="alert alert-success">
+		<p class="text-center align-middle">Selamat, Pesanan Anda telah Berhasil diproses!!!</p>
+	</div>
+>>>>>>> cde0bab563ae1e09e10f672bf65e94f1e9f8d13e
 </div>
